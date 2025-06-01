13027316541117a2781947db6cd2abc1c9a82291
Edson Vinicio Silva <edsonvinicio23p@gmail.com>
2025-06-01 15:31:43 -0300

    teste
    teste

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
-import estoque from "./estoque.js";
+const express = require('express');
+const fs = require('fs');
+const app = express();
+app.use(express.json());
+
+app.get('/notas', (req, res) => {
+  const notas = JSON.parse(fs.readFileSync('db.json'));
+  res.json(notas);
+});
+
+app.post('/notas', (req, res) => {
+  const notas = JSON.parse(fs.readFileSync('db.json'));
+  notas.push(req.body);
+  fs.writeFileSync('db.json', JSON.stringify(notas));
+  res.json({ success: true });
+});
+
+// Adicione PUT (editar) e DELETE aqui...
+app.listen(3000, () => console.log('Servidor rodando!'));
+
+/*import estoque from "./estoque.js";
 import express from "express";
 
 const app = express();
@@ -8,3 +28,4 @@ app.get("/produtos", (req, res) => {
 });
 
 app.listen(3000);
+*/
